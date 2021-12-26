6f605ea28ac0f59db868e58df6b470b071f014db
Omar A <omarsin@gmx.com>
2021-12-25 20:39:35 -0500

    Small update

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -9,7 +9,7 @@ namespace WP_Rig\WP_Rig;
 
 ?>
 
-<div class="site-info">
+<!-- <div class="site-info">
 	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp-rig' ) ); ?>">
 		<?php
 		/* translators: %s: CMS name, i.e. WordPress. */
@@ -25,4 +25,5 @@ namespace WP_Rig\WP_Rig;
 		the_privacy_policy_link( '<span class="sep"> | </span>' );
 	}
 	?>
-</div><!-- .site-info -->
+</div> -->
+<!-- .site-info -->
